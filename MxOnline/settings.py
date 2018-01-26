60cd8099378d8c7dcb064755f456d3631e240260
Ting <ticccccc@163.com>
2018-01-26 11:01:32 +0800

    添加app名称

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'courses',
+    'organization',
+    'operation'
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
